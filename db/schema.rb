7064eafaea4ec4510050b05d6f551021eacac401
kazu4049 <ble2@outlook.jp>
2021-04-16 17:12:16 +0900

    テーブル内修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_072641) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "aderesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
     t.integer "shipment_area_id", null: false
     t.string "city", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_072641) do
     t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_id"], name: "index_aderesses_on_record_id"
+    t.index ["record_id"], name: "index_addresses_on_record_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_072641) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "aderesses", "records"
+  add_foreign_key "addresses", "records"
   add_foreign_key "items", "users"
   add_foreign_key "records", "items"
   add_foreign_key "records", "users"
